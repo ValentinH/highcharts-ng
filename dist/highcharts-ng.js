c8b36440b4d28185560305f7c0e5a9cf41497964
Valentin Hervieu <valentin.hervieu@milanamos.com>
2015-02-05 16:30:56 +0100

    Regenerate dist files

--- dist/highcharts-ng.js
+++ dist/highcharts-ng.js
@@ -244,6 +244,7 @@ if (typeof module !== 'undefined' && typeof exports !== 'undefined' && module.ex
         };
         $timeout(function () {
           initChart();
+          chart.reflow();
           if (scope.disableDataWatch) {
             scope.$watchCollection('config.series', function (newSeries, oldSeries) {
               processSeries(newSeries);
